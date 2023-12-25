27de84aa30ec86671bf7b7462dd83b8ce1b7d445
Saidakbar Zafarov <saidakbar.dev@gmail.com>
2023-12-25 15:07:55 +0500

    changed server API

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = 'http://localhost:5000/api'
+export const API_URL = 'https://task-4-api-bt87.onrender.com/api'
 
 const $api = axios.create({
     withCredentials: true,
